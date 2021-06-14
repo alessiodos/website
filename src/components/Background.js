49e0f3bfeb21738edd792994fc70147f0858b6f1
catenaprotocol <85307588+catenaprotocol@users.noreply.github.com>
2021-06-15 00:48:28 +0200

    Add files via upload

---
+++ src/components/Background.js
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+
+class Background extends Component {
+
+
+    render () {
+        return (
+            <div id="background-wrap"> 
+          
+            </div>
+        )
+    }
+}
+
+export default Background
