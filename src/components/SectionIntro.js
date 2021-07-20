d66c8c099d66ebb775908c3333a68a1753f8b684
alessiodos <alessiodos@gmail.com>
2021-07-20 21:19:41 +0200

    Update Section info

--- src/components/SectionIntro.js
+++ src/components/SectionIntro.js
@@ -22,13 +22,18 @@ class SectionIntro extends Component {
                 </div>
                 <div id="intro-subtitle">
                     <p>
-                    The Ctena Protocol is a community driven, fair launched DeFi Token. Three simple functions
-                    occur during each trade: Reflection, LP Acquisition, and Burn.
+                    CATENA Protocol is a community driven, fair launched, DeFi token focused on offering advanced 
+                    DeFi tools and products to meet the growing user demand for tamper-proof, high-yield DeFi products. 
+                    $CTENA is a token on Binance Smart Chain boasting a number of impressive features.
+                    Three simple functions occur during each trade: Reflection, LP Acquisition, and Burn.
                     </p>
                 </div>
                 <div id="intro-buttons">
-                <a href="https://t.me/catenaprotocol"><button>Telegram</button></a>
-                <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x8f4fc37bff97e6e7fd4355a5df76a486ac1e2e1c"><button>Buy</button></a>
+                    <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x8f4fc37bff97e6e7fd4355a5df76a486ac1e2e1c"><button>Buy</button></a>
+                    <a href="https://t.me/catenaprotocol"><button>Telegram</button></a>
+                    <a href="https://twitter.com/CatenaProtocol"><button>Twitter</button></a>
+                    <a href="https://www.notion.so/7397a1c01e3b40d2889cba833a35f4fa?v=682b420b3c344f89a126c2df86506360"><button>Roadmap</button></a>
+                    <a href="https://catenaprotocol.medium.com/"><button>News</button></a>
                 </div>
        
 
